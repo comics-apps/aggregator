ae372e14717a075277a34068dbfc4331de7b6097
Krzysztof Wawer <krzysztof.wawer@gmail.com>
2018-06-10 13:31:47 +0200

    Add bootstrap social buttons and font-awesome

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,12 @@ import "../css/styles.scss"
 export default () => (
   <div>
     <p>Hello World!</p>
+
+    <div className="text-center">
+      <a class="btn btn-social btn-google">
+        <span class="fa fa-google"></span>
+        Sign in with Google
+      </a>
+    </div>
   </div>
 )
